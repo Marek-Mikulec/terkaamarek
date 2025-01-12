7c9f574e0af4622092d66df2b250ec44646dde28
MarekMikulec <marek.432@email.cz>
2025-01-12 08:54:19 +0100

    vytvořen header

--- script/script.js
+++ script/script.js
@@ -1,1 +1,5 @@
-console.log("AHOJ")
+window.addEventListener('scroll', function() {
+    let scrolled = window.scrollY;
+    let offset = scrolled * 0.3;
+    document.querySelector('header').style.backgroundPosition = `center calc(50% + ${offset}px)`;
+});
