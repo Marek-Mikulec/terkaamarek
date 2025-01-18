b79d6a1d6546e731155cedd4d363b352aa043461
MarekMikulec <marek.432@email.cz>
2025-01-18 12:51:22 +0100

    Připravené důležité informace + základ: potvrzení účasti, svatební program + příprava na monitory s vysokým rozlišením + změna fontu na úvodu

--- script/script.js
+++ script/script.js
@@ -1,5 +1,24 @@
+/**************** Header ****************/
+
+/** Scrolování úvodní obrazovky **/
+
 window.addEventListener('scroll', function() {
     let scrolled = window.scrollY;
     let offset = scrolled * 0.3;
     document.querySelector('header').style.backgroundPosition = `center calc(50% + ${offset}px)`;
+});
+
+/**************** Formulář ****************/
+
+/** Formulář input phone **/
+
+const phoneInput = document.getElementById('phone');
+
+phoneInput.addEventListener('input', function() {
+  // Povolit pouze číslice
+  phoneInput.value = phoneInput.value.replace(/\D/g, '');
+  // Zabránit zadání více než 9 číslic
+  if (phoneInput.value.length > 9) {
+    phoneInput.value = phoneInput.value.slice(0, 9);
+  }
 });
