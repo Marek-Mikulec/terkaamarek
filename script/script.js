76e80980ed02bc1cd2587e61444784e1a8c9469c
MarekMikulec <marek.432@email.cz>
2025-01-26 23:24:44 +0100

    Připravený formulář + lehce upravená responzibilita

--- script/script.js
+++ script/script.js
@@ -10,15 +10,30 @@ window.addEventListener('scroll', function() {
 
 /**************** Formulář ****************/
 
-/** Formulář input phone **/
+/** Formulář input phone a mezery mezi čísla  **/
 
 const phoneInput = document.getElementById('form-phone');
 
-phoneInput.addEventListener('input', function() {
-  // Povolit pouze číslice
-  phoneInput.value = phoneInput.value.replace(/\D/g, '');
-  // Zabránit zadání více než 9 číslic
-  if (phoneInput.value.length > 9) {
-    phoneInput.value = phoneInput.value.slice(0, 9);
-  }
+phoneInput.addEventListener('input', () => {
+    let value = phoneInput.value.replace(/\D/g, ''); // Povolit pouze čísla
+    if (value.length > 3 && value.length <= 6) {
+        value = `${value.slice(0, 3)} ${value.slice(3)}`;
+    } else if (value.length > 6) {
+        value = `${value.slice(0, 3)} ${value.slice(3, 6)} ${value.slice(6, 9)}`;
+    }
+    phoneInput.value = value;
+});
+
+/** Formulář input vyplněn alespoň jeden údaj **/
+
+const form = document.getElementById("formular-layout");
+
+form.addEventListener("submit", function(event) {
+    const email = document.getElementById("form-email").value.trim();
+    const phone = document.getElementById("form-phone").value.trim();
+
+    if (!email && !phone) {
+        event.preventDefault(); // Zabraň odeslání formuláře
+        alert("Vyplňte prosím alespoň e-mail nebo telefonní číslo.");
+    }
 });
