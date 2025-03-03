d4689073d152dbbe61bd30c12c4a5d7d93e2b9a2
MarekMikulec <marek.432@email.cz>
2025-03-03 21:09:38 +0100

    zmenšené obrázky + úprava odeslání formuláře + příprava na zobrazování při odeslat u formuláře

--- others/mailer.php
+++ others/mailer.php
@@ -20,9 +20,14 @@ $kids_ages_text = !empty($kids_ages) ? implode(", ", $kids_ages) : "Neuvedeno";
 
 // Kontrola nutných polí
 if (empty($name) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    header("Location: http://www.terka-a-marek.cz/index.php?success=-1#formular");
+    echo "<script>window.location.href = 'http://www.terka-a-marek.cz/index.php?success=-1#formular';</script>";
     exit;
 }
+
+// if (empty($name) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//     header("Location: http://www.terka-a-marek.cz/index.php?success=-1#formular");
+//     exit;
+// }
 
 // Nastavení příjemců
 $recipients = ["marek.432@email.cz", "terkach23@gmail.com"];
@@ -49,6 +54,8 @@ foreach ($recipients as $recipient) {
 }
 
 // Přesměrování po úspěšném odeslání
-header("Location: http://www.terka-a-marek.cz/index.php?success=1#formular");
+echo "<script>window.location.href = 'http://www.terka-a-marek.cz/index.php?success=1#formular';</script>";
+
+// header("Location: http://www.terka-a-marek.cz/index.php?success=1#formular");
 exit;
 ?>
